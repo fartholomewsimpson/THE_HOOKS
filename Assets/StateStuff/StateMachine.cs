efed68f6abc91f255b920982c666decb90f72b84
Noodleman <sherwoodwt@gmail.com>
2021-04-03 11:34:24 -0700

    get moving animation to work

--- Assets/StateStuff/StateMachine.cs
+++ Assets/StateStuff/StateMachine.cs
@@ -5,8 +5,12 @@ namespace StateStuff
 {
     public class StateMachine : MonoBehaviour
     {
-        private new Rigidbody2D rigidbody;
-        private State currentState;
+        new Rigidbody2D rigidbody;
+        State currentState;
+
+        // TODO: Move these if possible?
+        Animator animator;
+        SpriteRenderer spriteRenderer;
 
         public State CurrentState { get { return currentState; } }
 
@@ -14,11 +18,19 @@ namespace StateStuff
         {
             rigidbody = GetComponent<Rigidbody2D>();
             currentState = new StandingState();
+
+            animator = GetComponent<Animator>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         void FixedUpdate()
         {
             currentState = currentState.Update(rigidbody);
+            animator.SetFloat("Velocity", Mathf.Abs(rigidbody.velocity.x));
+            if (rigidbody.velocity.x > 0)
+                spriteRenderer.flipX = false;
+            if (rigidbody.velocity.x < 0)
+                spriteRenderer.flipX = true;
         }
 
         void OnCollisionEnter2D(Collision2D collision)
