c1bf1e962a9ea854a6d1fc132e3f5317a3cb7c48
Noodleman <sherwoodwt@gmail.com>
2021-04-28 00:36:57 -0700

    corner bug fix

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,7 +8,7 @@ public class Controller : MonoBehaviour
     public int rayCount = 4;
     public LayerMask collisionMask;
 
-    public event Action onVerticalCollision, onHorizontalCollision;
+    public event Action<RaycastHit2D> onVerticalCollision, onHorizontalCollision;
 
     float paddingWidth = .05f;
     new BoxCollider2D collider;
@@ -20,6 +20,7 @@ public class Controller : MonoBehaviour
         public Vector2 end;
     }
 
+    // TODO: Rather than having a public move function, maybe this should be a component and just move on fixedupdate?
     public Vector2 Move(Vector2 velocity) {
         CalculateCorners();
         if (Mathf.Abs(velocity.y) > 0)
@@ -49,30 +50,7 @@ public class Controller : MonoBehaviour
         Debug.DrawLine(bottomLeft, topLeft);
     }
 
-    void DetectHorizontalCollisions(ref Vector2 velocity) {
-        var sign = Mathf.Sign(velocity.x);
-        var pair = sign > 0
-            ? new CornerPair { start = bottomRight, end = topRight }
-            : new CornerPair { start = bottomLeft, end = topLeft };
-        var spacing = (pair.end.y - pair.start.y) / (rayCount - 1);
-        var direction = Vector2.right * sign;
-        var distance = Mathf.Abs(velocity.x) + paddingWidth;
-
-        for (int i = 0; i < rayCount; i++) {
-            var origin = new Vector2(pair.start.x, pair.start.y + (spacing * i));
-            Debug.DrawRay(origin, direction * distance, Color.red);
-
-            var hit = Physics2D.Raycast(origin, direction, distance, collisionMask);
-            if (hit) {
-                velocity.x = (hit.distance - paddingWidth) * sign;
-                Debug.DrawRay(origin, direction * distance, Color.blue);
-                distance = hit.distance;
-                if (onHorizontalCollision != null)
-                    onHorizontalCollision();
-            }
-        }
-    }
-
+    // TODO: Break out core logic
     void DetectVerticalCollisions(ref Vector2 velocity) {
         var sign = Mathf.Sign(velocity.y);
         var pair = sign > 0
@@ -92,7 +70,32 @@ public class Controller : MonoBehaviour
                 Debug.DrawRay(origin, direction * distance, Color.blue);
                 distance = hit.distance;
                 if (onVerticalCollision != null)
-                    onVerticalCollision();
+                    onVerticalCollision(hit);
+            }
+        }
+    }
+
+    void DetectHorizontalCollisions(ref Vector2 velocity) {
+        var sign = Mathf.Sign(velocity.x);
+        var pair = sign > 0
+            ? new CornerPair { start = bottomRight, end = topRight }
+            : new CornerPair { start = bottomLeft, end = topLeft };
+        var spacing = (pair.end.y - pair.start.y) / (rayCount - 1);
+        var direction = Vector2.right * sign;
+        var distance = Mathf.Abs(velocity.x) + paddingWidth;
+
+        for (int i = 0; i < rayCount; i++) {
+            // velocity.y added here since horizontal is calculated 2nd. This is to fix the corner cliping problem
+            var origin = new Vector2(pair.start.x, pair.start.y + (spacing * i) + velocity.y);
+            Debug.DrawRay(origin, direction * distance, Color.red);
+
+            var hit = Physics2D.Raycast(origin, direction, distance, collisionMask);
+            if (hit) {
+                velocity.x = (hit.distance - paddingWidth) * sign;
+                Debug.DrawRay(origin, direction * distance, Color.blue);
+                distance = hit.distance;
+                if (onHorizontalCollision != null)
+                    onHorizontalCollision(hit);
             }
         }
     }
